b7b98a613f303bcd6757044b0dd5b5a2e254ede2
Anthony Sena <asena5@its.jnj.com>
2024-05-02 17:03:00 -0400

    WIP

--- inst/sql/sql_server/ResultTables.sql
+++ inst/sql/sql_server/ResultTables.sql
@@ -3,10 +3,11 @@ CREATE TABLE @my_schema.@table_prefixtime_to_event (
     target_cohort_definition_id bigint NOT NULL,
     outcome_cohort_definition_id bigint NOT NULL,
     outcome_type varchar(20) NOT NULL,
-    target_outcome_type varchar(20) NOT NULL,
+    target_outcome_type varchar(40) NOT NULL,
     time_to_event int NOT NULL,
     num_events int NOT NULL,
-    time_scale varchar(20) NOT NULL
+    time_scale varchar(20) NOT NULL,
+    PRIMARY KEY (database_id, target_cohort_definition_id, outcome_cohort_definition_id, outcome_type, target_outcome_type)
 );
 
 CREATE TABLE @my_schema.@table_prefixrechallenge_fail_case_series (
@@ -27,7 +28,8 @@ CREATE TABLE @my_schema.@table_prefixrechallenge_fail_case_series (
     rechallenge_exposure_start_date_offset int NOT NULL,
     rechallenge_exposure_end_date_offset int NOT NULL,
     rechallenge_outcome_number int NOT NULL,
-    rechallenge_outcome_start_date_offset int NOT NULL
+    rechallenge_outcome_start_date_offset int NOT NULL,
+    PRIMARY KEY (database_id, target_cohort_definition_id, outcome_cohort_definition_id, person_key)
 );
 
 CREATE TABLE @my_schema.@table_prefixdechallenge_rechallenge (
@@ -50,7 +52,8 @@ CREATE TABLE @my_schema.@table_prefixdechallenge_rechallenge (
     pct_dechallenge_fail float,
     pct_rechallenge_attempt float,
     pct_rechallenge_success float,
-    pct_rechallenge_fail float
+    pct_rechallenge_fail float,
+    PRIMARY KEY (database_id, dechallenge_stop_interval, dechallenge_evaluation_window, target_cohort_definition_id, outcome_cohort_definition_id)
 );
 
 
@@ -62,17 +65,19 @@ CREATE TABLE @my_schema.@table_prefixsettings (
     risk_window_start int,
     start_anchor varchar(15),
     risk_window_end int,
-    end_anchor varchar(15)
+    end_anchor varchar(15),
+    PRIMARY KEY (run_id, database_id)
 );
 
 -- added this table
 CREATE TABLE @my_schema.@table_prefixcohort_details (
     run_id int NOT NULL,
     database_id varchar(100),
-    cohort_definition_id int,
+    cohort_definition_id bigint,
     target_cohort_id int,
     outcome_cohort_id int,
-    cohort_type varchar(10)
+    cohort_type varchar(10),
+    PRIMARY KEY (run_id, database_id)
 );
 
 CREATE TABLE @my_schema.@table_prefixanalysis_ref (
@@ -84,32 +89,35 @@ CREATE TABLE @my_schema.@table_prefixanalysis_ref (
     start_day int,
     end_day int,
     is_binary varchar(1),
-    missing_means_zero varchar(1)
+    missing_means_zero varchar(1),
+    PRIMARY KEY (database_id, run_id)
 );
 
 CREATE TABLE @my_schema.@table_prefixcovariate_ref (
     run_id int NOT NULL,
     database_id varchar(100) NOT NULL,
-    covariate_id int NOT NULL,
+    covariate_id bigint NOT NULL,
     covariate_name varchar(max) NOT NULL,
     analysis_id int NOT NULL,
-    concept_id int
+    concept_id bigint,
+    PRIMARY KEY (database_id, run_id, covariate_id)
 );
 
 CREATE TABLE @my_schema.@table_prefixcovariates (
     run_id int NOT NULL,
     database_id varchar(100) NOT NULL,
-    cohort_definition_id int NOT NULL,
-    covariate_id int NOT NULL,
+    cohort_definition_id bigint NOT NULL,
+    covariate_id bigint NOT NULL,
     sum_value int NOT NULL,
-    average_value float NOT NULL
+    average_value float NOT NULL,
+    PRIMARY KEY (database_id, run_id, cohort_definition_id, covariate_id)
 );
 
 CREATE TABLE @my_schema.@table_prefixcovariates_continuous (
     run_id int NOT NULL,
     database_id varchar(100) NOT NULL,
-    cohort_definition_id int NOT NULL,
-    covariate_id int NOT NULL,
+    cohort_definition_id bigint NOT NULL,
+    covariate_id bigint NOT NULL,
     count_value int NOT NULL,
     min_value float,
     max_value float,
@@ -119,13 +127,15 @@ CREATE TABLE @my_schema.@table_prefixcovariates_continuous (
     p_10_value float,
     p_25_value float,
     p_75_value float,
-    p_90_value float
+    p_90_value float,
+    PRIMARY KEY (database_id, run_id, cohort_definition_id, covariate_id)
 );
 
 CREATE TABLE @my_schema.@table_prefixcohort_counts(
     run_id int NOT NULL,
     database_id varchar(100) NOT NULL,
-    cohort_definition_id int NOT NULL,
+    cohort_definition_id bigint NOT NULL,
     row_count int NOT NULL,
-    person_count int NOT NULL
+    person_count int NOT NULL,
+    PRIMARY KEY (run_id, database_id)
 );
