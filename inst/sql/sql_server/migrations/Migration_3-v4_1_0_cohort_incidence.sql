0328319de1f267c684a3bcdfe7934cd61de9644a
jreps <jreps@its.jnj.com>
2026-09-02 13:14:55 -0400

    fixing results tables for CI

--- inst/sql/sql_server/migrations/Migration_3-v4_1_0_cohort_incidence.sql
+++ inst/sql/sql_server/migrations/Migration_3-v4_1_0_cohort_incidence.sql
@@ -9,6 +9,7 @@
 -- ===========================
 --HINT DISTRIBUTE ON RANDOM
 CREATE TABLE @database_schema.@table_prefixincidence_summary(
+  setting_id varchar(50),
   ref_id INT,
   database_id VARCHAR(255),
   source_name VARCHAR(255),
@@ -29,8 +30,8 @@ CREATE TABLE @database_schema.@table_prefixincidence_summary(
   person_outcomes BIGINT,
   outcomes_pe BIGINT,
   outcomes BIGINT,
-  incidence_proportion_p100p FLOAT,
-  incidence_rate_p100py FLOAT
+  incidence_proportion_p_100p FLOAT,
+  incidence_rate_p_100py FLOAT
   --PRIMARY KEY (ref_id, database_id, characterization_target_id, tar_id, subgroup_id, outcome_id,
   -- age_group_id, gender_id, start_year)
 );
@@ -38,6 +39,7 @@ CREATE TABLE @database_schema.@table_prefixincidence_summary(
 
 
 CREATE TABLE @database_schema.@table_prefixtarget_def(
+  setting_id varchar(50),
   ref_id INT,
   --target_cohort_definition_id BIGINT,
   characterization_target_id BIGINT,
@@ -46,6 +48,7 @@ CREATE TABLE @database_schema.@table_prefixtarget_def(
 );
 
 CREATE TABLE @database_schema.@table_prefixoutcome_def(
+  setting_id varchar(50),
   ref_id INT,
   outcome_id BIGINT,
   outcome_cohort_definition_id BIGINT,
@@ -56,6 +59,7 @@ CREATE TABLE @database_schema.@table_prefixoutcome_def(
 );
 
 CREATE TABLE @database_schema.@table_prefixtar_def(
+  setting_id varchar(50),
   ref_id INT,
   tar_id BIGINT,
   tar_start_with VARCHAR(10),
@@ -66,6 +70,7 @@ CREATE TABLE @database_schema.@table_prefixtar_def(
 );
 
 CREATE TABLE @database_schema.@table_prefixage_group_def(
+  setting_id varchar(50),
   ref_id INT,
   age_group_id INT,
   age_group_name VARCHAR(255),
@@ -75,6 +80,7 @@ CREATE TABLE @database_schema.@table_prefixage_group_def(
 );
 
 CREATE TABLE @database_schema.@table_prefixsubgroup_def(
+  setting_id varchar(50),
   ref_id INT,
   subgroup_id BIGINT,
   subgroup_name VARCHAR(255),
@@ -82,6 +88,7 @@ CREATE TABLE @database_schema.@table_prefixsubgroup_def(
 );
 
 CREATE TABLE @database_schema.@table_prefixtarget_outcome_ref(
+  setting_id varchar(50),
   ref_id INT,
   --target_cohort_id BIGINT,
   characterization_target_id BIGINT,
@@ -89,3 +96,7 @@ CREATE TABLE @database_schema.@table_prefixtarget_outcome_ref(
   PRIMARY KEY (ref_id, characterization_target_id, outcome_cohort_id)
 );
 
+
+-- add cohort_incidence_settings to target_settings
+ALTER TABLE  @database_schema.@table_prefixtarget_settings
+ADD COLUMN cohort_incidence_settings CHAR(1);
